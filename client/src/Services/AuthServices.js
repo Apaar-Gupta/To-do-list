0fc57177232f1ec953d529df4cbb3276d15fc9ec
Apaar-Gupta <apaarmsd@gmail.com>
2026-01-16 16:33:59 +0530

    Auth updated

--- client/src/Services/AuthServices.js
+++ client/src/Services/AuthServices.js
@@ -3,17 +3,11 @@ import axios from "axios";
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
-const registerUser = (data) => {
-  return axios.post("/api/v1/user/register", data);
-};
 
-const loginUser = (data) => {
-  return axios.post("/api/v1/user/login", data);
-};
+export const registerUser = (data) =>
+  API.post("/api/v1/user/register", data);
 
-const AuthServices = {
-  registerUser,
-  loginUser,
-};
+export const loginUser = (data) =>
+  API.post("/api/v1/user/login", data);
 
-export default AuthServices;
+export default { registerUser, loginUser };
--- client/src/Services/TodoServices.js
+++ client/src/Services/TodoServices.js
@@ -1,39 +1,28 @@
 import axios from "axios";
 
-
 const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
 
-const user = JSON.parse(localStorage.getItem("todoapp"));
+// attach token
+API.interceptors.request.use((req) => {
+  const user = JSON.parse(localStorage.getItem("todoapp"));
+  if (user?.token) {
+    req.headers.Authorization = `Bearer ${user.token}`;
+  }
+  return req;
+});
 
-if (user?.token) {
-  axios.defaults.headers.common["Authorization"] = `Bearer ${user.token}`;
-}
+export const createTodo = (data) => API.post("/api/v1/todo/create", data);
+export const getAllTodo = (id) => API.get(`/api/v1/todo/getAll/${id}`);
+export const updateTodo = (id, data) =>
+  API.patch(`/api/v1/todo/update/${id}`, data);
+export const deleteTodo = (id) =>
+  API.delete(`/api/v1/todo/delete/${id}`);
 
-
-
-const createTodo = async (data) => {
-  return axios.post("/api/v1/todo/create", data);
-};
-
-const getAllTodo = async (id) => {
-  return axios.get(`/api/v1/todo/getAll/${id}`);
-};
-
-const updateTodo = async (id, data) => {
-  return axios.patch(`/api/v1/todo/update/${id}`, data);
-};
-
-const deleteTodo = async (id) => {
-  return axios.delete(`/api/v1/todo/delete/${id}`);
-};
-
-const TodoServices = {
+export default {
   createTodo,
   getAllTodo,
   updateTodo,
   deleteTodo,
 };
-
-export default TodoServices;
